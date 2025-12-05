55d9024940b2b8912321e355ba63475de2c3d327
reginazorrilla <reginazorrillag@gmail.com>
2025-12-05 13:51:13 -0600

    Update tictactoe.cpp
    c/p the thing stright from the file cuz it wasn't letting me commit the changes

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
-
+#include "functions.h"
 
 int main(){
+    cout << "Welcome to Trivia-Tac-Toe." << endl;
+    cout << "In this Tic-Tac-Toe game, you'll have to answer a question correctly."<< endl;
+    cout << "There are 5 different genres of questions: English, History, Coding(C++), Science, and Pop Culture." << endl;
+    cout << "The computer will randomize the genre each turn." << endl;
+    cout << "Starting Board" << endl;
+    printBoard();
+do{
+    randomizeGenre();
 
-    return 0;
+
+
+
 }
-
-
-// FUNCTION DEFINITIONS
+    return 0;
+}  
