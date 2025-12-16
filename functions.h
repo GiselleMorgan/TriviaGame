f40e537416cf41557a7e58ee2f7cc73d5fdaa8b8
GiselleMorgan <gisellenmorgan@gmail.com>
2025-12-16 13:35:03 -0600

    fxing function, fixing main, deleting class file

--- functions.h
+++ functions.h
@@ -23,34 +23,34 @@ void printBoard(){//this function prinnts the board using nested for loops and a
 
 void randomizeGenre(){
 int randNum =  rand() % 5 + 1; //this SHOULD generate a random number between 1 and 5
-if (randNum == 1){ //if random number is 1, call englishq() and so on...
-    englishq();
+if (randNum == 1){ //if random number is 1, return englishq() and so on...
+    return englishq();
 } else if (randNum == 2){
-    historyq();
+    return historyq();
 } else if (randNum == 3){
-    codingq();
+    return codingq();
 } else if (randNum == 4){
-    scienceq();
+    return scienceq();
 } else {
-    popq();
+    return popq();
 }
 }
 
 //format to write it in: {"Question", "A", "B", "C", "D", "Correct Answer"}
-string englishq(){
-string englishques[5][6] =
+string* englishq(){
+static string englishques[5][6] =
 {"Do you have any idea what she ___ at this time tomorrow?", "A. has done" , "B. Will be doing",  "C. Was doing",  "D. Is doing", "B"},
 {"What is the opposite of big?", "A. Loose",  "B. Little" "C. Long", "D. Large", "B"},
 {"What does a sentence end with?", "A. Capital Letter", "B. Adverb", "C. Subject", "D. Subject", "D"},
 {"Which kind of sentence is the following? \nThe students are laughing at the video.", "A. Statement", "B. Command", "C. Question", "D. Exclamation", "A"},
 {"What is a verb?", "A. Person, place, or thing", "B. Word that replaces a noun", "C. Action word", "D. Connects different parts of a sentence", "C"};
 int randNum =  rand() % 5; //should generate a random number between 0 and 4
-return englishques[randNum]; 
+return englishques[randNum]; //return the whole row of randNum
 
 
 };
-string historyq(){
-string historyques[5][6]=
+string* historyq(){
+static string historyques[5][6]=
 {"Who, at age 17, became the youngest person to win a Nobel Peace Prize in 2014?", "A. Lawrence Bragg", "B. Malala Yousafzai", "C. Werner Heisenberg", "D. Paul Dirac", "B"},
 {"What was the name of the ancient trade route that connected the East with the West?", "A. The Spice Routes", "B. The Amber Road", "C. The Silk Road", "D. The Incense Route", "C"},
 {"Which ancient civilization built Machu Picchu?", "A. Aztec", "B. Inca", "C. Maya", "D. Olmec", "B"}, ,
@@ -58,60 +58,61 @@ string historyques[5][6]=
 {"What was the name of the battle in the Pacific fought between the U.S. Navy and the Imperial Japanese Navy in June 1942?", ""},
 {"How old was Queen Elizabeth II when she became the queen of England?", "A. 31", "B. 25", "C. 28", "D. 30", "B"};
 int randNum =  rand() % 5; //should generate a random number between 0 and 4
-return historyques[randNum]; //SHOULD print the randNum row's question
+return historyques[randNum]; //return the whole row of randNum
 };
-void codingq(){
-string codingques[5][6]=
+string* codingq(){
+static string codingques[5][6]=
 {"What is a correct syntax to output (Hello World)" ,"A. Console.WriteLine(Hello World)", "B.System.out.printIn(Hello World)"," C. print (Hello World), " "D. cout << (Hello World);","D")},,
 {"Which of the following operators hasd the highest precedence?", "A. !", "B. *", "C. %", "D. =", "A"},
 {"To use the predefined function fmod, the program must include the header file ___.", "A. <cctype>", "B. <iostream>", "C. <cmath>", "D. <cstdlib>", "C"},
 {"The output of the staements: cout<<pow(2.0,pow(3.0,1.0)); is ____.", "A. 6.0", "B. 7.0", "C. 8.0", "D. 9.0", "C"},
 {"Which executed first in a do...while loop?", "A. The body of the loop", "B. The increment statement", "C. "};
 int randNum =  rand() % 5; //should generate a random number between 0 and 4
-return codingques[randNum]; //SHOULD print the randNum row's question
+return codingques[randNum]; //return the whole row of randNum
 };
-void scienceq(){
-string scienceques[5][6]=
+string* scienceq(){
+static string scienceques[5][6]=
 {"What two mammals lay eggs?", "A. Bats and Squirrels", "B. Platypus and Bats", "C. Squirrels and Enchida", "D. Platpus and Enchidna", "D"},
 {"What is the largest ocean on Earth?", "A. The Atlantic Ocean", "B.The Indian Ocean", "C. The Pacific Ocean", "D. The Southern Ocean", "C"},
 {"What is the largest desert on Earth?","A. Arabia", "B.Sahara", "C. Kalahari", "D.Antartica","D" },
 {"What gives human hair, skin, and nails their color?","A. Keratin", "B. Melatonin", "C. Glucose", "D. Melanin","D"},
 {"What is the largest organ in the human body?","A. Skin", "B, Heart", "C. Stomach", "D. Lungs", "A"};
 int randNum =  rand() % 5; //should generate a random number between 0 and 4
-return scienceques[randNum]; //SHOULD print the randNum row's question
+return scienceques[randNum]; //return the whole row of randNum
 };
-void popq(){
-string popques[5][6]=
+string* popq(){
+static string popques[5][6]=
 {"Which actor voiced Mufasa in The Lion King and Darth Vader in the original Star Wars trilogy?", "A. James Earl Jones", "B. Eddie Murphy", "C. Jesse McCartney", "D. Christopher Borger","A"},
 {"What is Taylor Swift’s most popular album?", "A.. Evermore", "B. Red"},
 {"What high school subject did Walter White teach in Breaking Bad?","A. Math", "B. History", "C. Chemistry", "D. English", "C"},
 {"What musical features the characters Elphaba, Galinda, and Fiyero?", "A. Hamilton", "B. Chicago","C. Wicked","D. The Phantom of the Opera","C"},
 {"Which Netflix series follows a group of teenagers that takes place in the 80s?","A. You", "B. Ginny & Georgia", "C. Outer Banks", "D. Stranger Things","D"};
 int randNum =  rand() % 5; //should generate a random number between 0 and 4
-return popques[randNum]; //SHOULD print the randNum row's question
+return popques[randNum]; //return the whole row of randNum
 };
 
-void askQuestion(string question){
-    cout << question[0];
-    cout << question[1];
-    cout << question[2];
-    cout << question[3];
-    cout << question[4];
+void askQuestion(string* question){
+    cout << question[0] << endl; //prints the question
+    cout << question[1]<< endl; //prints answer choices
+    cout << question[2]<< endl;
+    cout << question[3]<< endl;
+    cout << question[4]<< endl;
     cout << endl;
-    cout << "Enter your answer(A, B, C, D): " << endl;
+    cout << "Enter your answer(A, B, C, D): " << endl; //prompts user to enter answer choice by just the letter
 }
-void checkAnswer(){
+bool checkAnswer(string* question){
     char answer;
-    cin >> answer;
-if (answer == question[5]){
+    cin >> answer; //get answer from user and store it
+if (answer == question[5]){//check if the answer entered is the same as the 6th column of the row returned
     cout << "Correct!";
-    placeMark();
+    return true; 
 }
 else {
     cout << "Incorrect."
+    return false;
 }}
 
-void placeMark(){
+void placeMark(char mark){
     int row, col;
     char mark;
     cout << "Enter the coordinates you'd like to place your mark on. Example: 0 0" << endl;
@@ -120,18 +121,11 @@ void placeMark(){
         cout << "Please input valid coordinates: " << endl;
         cin  >> row >> col;
     }
-    cout << "Enter what mark you'd like to place (X or O): ";
-    cout << endl;
-    cin >> mark;
-    while (mark !='O' && mark !='X'){ //input validation for mark
-    cout << "Please input a valid mark: " << endl;
-    cin >> mark;
-}
+
     board[row][col]=mark;
-    printBoard();
 }
 
-void checkWin(){
+bool checkWin(){
     bool win;
     if ((board[0][0] == 'X')&& (board[1][1] == 'X')  && (board[2][2] == 'X')||(board[0][0] == 'X')&& (board[1][0] == 'X')  && (board[2][0] == 'X')|| (board[0][1] == 'X')&& (board[1][1] == 'X')  && (board[2][1] == 'X') || (board[0][2] == 'X')&& (board[1][2] == 'X')  && (board[2][2] == 'X') || (board[0][0] == 'X')&& (board[0][1] == 'X')  && (board[0][2] == 'X') || (board[1][0] == 'X')&& (board[1][1] == 'X')  && (board[1][2] == 'X') || (board[2][0] == 'X')&& (board[2][1] == 'X')  && (board[2][2] == 'X') ){
         win = true;
@@ -142,6 +136,7 @@ void checkWin(){
     else {
         win = false;
     }
+    return win;
 }
 
 
--- tictactoe.cpp
+++ tictactoe.cpp
@@ -6,31 +6,43 @@ using namespace std;
 int main(){
     cout << "Welcome to Trivia-Tac-Toe." << endl;
     cout << "In this Tic-Tac-Toe game, you'll have to answer a question correctly."<< endl;
-    cout << "There are 5 different genres of questions: English, History, Coding(C++), Science, and Pop Culture." << endl;
+    cout << "There are 5 different genres of questions: English, History, Coding, Science, and Pop Culture." << endl;
     cout << "The computer will randomize the genre each turn." << endl;
-    cout << "Starting Board" << endl;
+    cout << "Starting Board: " << endl;
+    do {
     createBoard(); //initalize board with dashes
     printBoard(); //print the board
 
-do { //do while
-    askQuestion(); //ask question, randomize genre, randomize number for question, print question and answers
-    checkAnswer(); //
-    if (checkAnswer()==1){
-    placeMark();
+    bool game = false;
+    char currentPlayer = 'X'
+    while (!game){
+        cout << "Player " << currentPlayer << "'s turn." << endl;
+        string* q = randomizeGenre();
+        askQuestion(q);
+
+        if (checkAnswer(q)){
+            placeMark(currentPlayer);
+            printBoard();
+            game = checkWin();
+        } else{
+            cout << "You missed the question! No mark placed" << endl;
+        }
+        if (currentPlayer=='X'){
+            currentPlayer=='O';
+        } else {
+            currentPlayer=='X';
+        }
     }
     do{
-    string cont;  
+    char cont;  
         cout<< "Do you want to continue? \n[Y/N]"<<endl;
         cin>> cont;
-        };
-    while (cont != "Y"||"N")
-    if (cont == "N"){
+        } while (cont != 'Y'||'N');
+    if (cont == 'N'){
         break;
-    else if (cont == )
     }
-    }
-  
- 
+}while (cont=='Y');
+
     return 0;
 }  
 
